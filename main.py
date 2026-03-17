d3f5e19dd0ccccda260dbd3354b64ccd0c8abe88
hemant-singh-97 <97.singh.hemant@gmail.com>
2026-03-17 17:38:52 +0530

    learned Pyndatic and CRUD Operations

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from fastapi import Body
+# from fastapi import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Always keeps the API endpoints as plural, for example, /posts instead of /post. This is a good practice to follow when designing RESTful APIs.
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root() -> dict[str, str]:
@@ -15,12 +24,10 @@ def get_posts() -> dict[str, list[dict[str, str | int]]]:
     return {
         "data": [
             {
-                "id": 1,
                 "title": "First Post",
                 "content": "This is the content of the first post."
             },
             {
-                "id": 2,
                 "title": "Second Post",
                 "content": "This is the content of the second post."
             }
@@ -28,8 +35,21 @@ def get_posts() -> dict[str, list[dict[str, str | int]]]:
     }
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
+def create_posts(post: Post):
+    # From payload, we need onlt 2 fields, title as a string and content as a string.
+    print(post)
+    print(post.model_dump())
     return {
-        "new_post": f"title: {payload['title']}, content: {payload['content']}"
+        "new_post": f"title: {post.title}, content: {post.content}, published: {post.published}, rating: {post.rating}"
     }
+
+"""
+CRUD Operations:
+1. Create: POST /posts (app.post("/posts"))
+2. Read: GET /posts (app.get("/posts"))
+3. Update: PUT /posts/{id} (app.put("/posts/{id}")) OR PATCH /posts/{id} (app.patch("/posts/{id}"))
+The difference in PUT and PATCH is that PUT is used to update the entire resource, while PATCH is used to update only a part of the resource.
+For example, if we have a post with title and content, and we want to update only the title, we can use PATCH /posts/{id} with the new title in the request body.
+If we want to update both the title and content, we can use PUT /posts/{id} with the new title and content in the request body.
+4. Delete: DELETE /posts/{id} (app.delete("/posts/{id}"))
+"""
