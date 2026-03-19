2c5d5222e3d4dcdaf7164ffe7a9b56a906cffa16
hemant-singh-97 <97.singh.hemant@gmail.com>
2026-03-20 00:41:10 +0530

    placed the comments in right file

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,6 +11,17 @@ router = APIRouter(
     tags = ["Posts"]
 )
 
+"""
+CRUD Operations:
+1. Create: POST /posts (app.post("/posts"))
+2. Read: GET /posts (app.get("/posts"))
+3. Update: PUT /posts/{id} (app.put("/posts/{id}")) OR PATCH /posts/{id} (app.patch("/posts/{id}"))
+The difference in PUT and PATCH is that PUT is used to update the entire resource, while PATCH is used to update only a part of the resource.
+For example, if we have a post with title and content, and we want to update only the title, we can use PATCH /posts/{id} with the new title in the request body.
+If we want to update both the title and content, we can use PUT /posts/{id} with the new title and content in the request body.
+4. Delete: DELETE /posts/{id} (app.delete("/posts/{id}"))
+"""
+
 @router.get("/", response_model = list[schemas.PostResponse]) # response_model is used to specify the model of the response, it will automatically convert the response to the specified model and return it in the response body.
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
--- app/routers/user.py
+++ app/routers/user.py
@@ -11,17 +11,6 @@ router = APIRouter(
     tags = ["Users"]
 )
 
-"""
-CRUD Operations:
-1. Create: POST /posts (app.post("/posts"))
-2. Read: GET /posts (app.get("/posts"))
-3. Update: PUT /posts/{id} (app.put("/posts/{id}")) OR PATCH /posts/{id} (app.patch("/posts/{id}"))
-The difference in PUT and PATCH is that PUT is used to update the entire resource, while PATCH is used to update only a part of the resource.
-For example, if we have a post with title and content, and we want to update only the title, we can use PATCH /posts/{id} with the new title in the request body.
-If we want to update both the title and content, we can use PUT /posts/{id} with the new title and content in the request body.
-4. Delete: DELETE /posts/{id} (app.delete("/posts/{id}"))
-"""
-
 @router.post("/", status_code = status.HTTP_201_CREATED, response_model = schemas.UserResponse)
 def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
